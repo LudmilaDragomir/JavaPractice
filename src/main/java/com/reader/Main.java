338f4625022f63221f00043ffbea60dbe0713db1
Ludmila Dragomir <ludmila.dragomir@deeperthanblue.co.uk>
2022-09-21 14:27:47 +0100

    equal vs ==

--- src/main/java/com/reader/Main.java
+++ src/main/java/com/reader/Main.java
@@ -1,10 +1,17 @@
 package com.reader;
 
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hello world from IntelliJ");
-// comment
+        File x = new File("myFile.txt");
+        File y = new File("myFile.txt");
+
+        System.out.println(x == y);
+        System.out.println(x.equals(y));
+        // comment
     }
 }
